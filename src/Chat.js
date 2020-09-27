0c7e093a3ccccddc79d02a0377cd21a4c8d17dca
Vinícius Vargas Ribeiro <vinicius.vargas@conectt.com.br>
2020-09-27 20:08:54 -0300

    Criado o componente de chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,48 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React from 'react'
+import './Chat.css'; 
 
 function Chat() {
-    return <div className="chat">
-            <h1>I am the chat</h1>
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Lest seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Nea</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+                <p className="chat__message">
+                    <span className="chat__name">Nea</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+                <p className="chat__message">
+                    <span className="chat__name">Nea</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
         </div>
+    );
 }
 
 export default Chat
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,34 +5,39 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
 
 const Sidebar = () => {
-    return <div className="sidebar">        
-        <div className="sidebar__header">
-            <Avatar src='https://avatars1.githubusercontent.com/u/44420618?s=460&u=49054a98643dbaa57497529ecb0ba08ebef234f1&v=4'/>
-            <div className="sidebar__headerRight">
-                <IconButton>
-                    <DonutLargeIcon/>
-                </IconButton>
-                <IconButton>
-                    <ChatIcon/>
-                </IconButton>
-                <IconButton>
-                    <MoreVertIcon/>
-                </IconButton>
+    return (
+        <div className="sidebar">        
+            <div className="sidebar__header">
+                <Avatar src='https://avatars1.githubusercontent.com/u/44420618?s=460&u=49054a98643dbaa57497529ecb0ba08ebef234f1&v=4'/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div>
+            </div>
+            <div className="sidebar__chats">
+               <SidebarChat />
+               <SidebarChat />
+               <SidebarChat />
             </div>
         </div>
-        <div className="sidebar__search">
-            <div className="sidebar__searchContainer">
-                <SearchOutlined />
-                <input placeholder="Search or start new chat" type="text"/>
-            </div>
-        </div>
-        <div className="sidebar__chats">
-
-        </div>
-    </div>;
+    );
 }
 
 export default Sidebar;
