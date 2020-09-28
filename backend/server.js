aa679b96f611a9ce514ec414f5a5b46b38a7083d
Vinícius Vargas Ribeiro <vinicius.vargas@conectt.com.br>
2020-09-27 23:06:33 -0300

    removed submodule

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 9000
+
+app.get('/', (req, res)=>{
+    res.status(200).send('hello world')
+})
+
+app.listen(port, ()=>{
+    console.log(`Listening on localhost:${port}`)
+})
